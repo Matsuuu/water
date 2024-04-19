8118d3fe748b002319e150063d1af2be0569ec3b
Matsuuu <huhta.matias@gmail.com>
2024-04-19 22:21:56 +0300

    Reading ranges ok

--- test/selection.test.js
+++ test/selection.test.js
@@ -3,10 +3,16 @@ import * as fs from "fs";
 import assert from 'node:assert/strict';
 import { readFileRange } from "../src/file/file.js";
 
-test("Reading file ranges", async () => {
-    const BASIC_FIXTURE = "./fixtures/basic.fixture.js"
+const BASIC_FIXTURE = "./fixtures/basic.fixture.js"
 
+test("Reading file ranges", async () => {
     const fileOutput = await readFileRange(BASIC_FIXTURE)
 
-    assert(fileOutput, fs.readFileSync(BASIC_FIXTURE, { encoding: "utf8" }));
+    assert.equal(fileOutput, fs.readFileSync(BASIC_FIXTURE, { encoding: "utf8" }));
 })
+
+test("Reading file range", async () => {
+    const fileOutput = await readFileRange(BASIC_FIXTURE, { start: 10, end: 28 });
+
+    assert.equal(fileOutput, "thing = \"Something\"");
+});
