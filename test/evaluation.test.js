117845f6ff855e0710683fcd62e314c850d5e02a
Matsuuu <huhta.matias@gmail.com>
2024-04-21 13:04:06 +0300

    Prepare evaluation test cases

---
+++ test/evaluation.test.js
@@ -0,0 +1,38 @@
+import test from "node:test";
+
+
+test('File evaluation executes successfully', () => {
+
+});
+
+test("File evaluation is remembered on next calls", () => {
+
+});
+
+test("Range evaluation executes the code accordingly, returning evaluation result", () => {
+
+});
+
+test("Line evaluation executes the code accordingly, returning evaluation result", () => {
+
+});
+
+test("Function evaluation executes the code accordingly, saving function for later calls", () => {
+
+});
+
+test("Function content evaluation executes the code accordingly, returning evaluation result", () => {
+
+});
+
+test("Function call evaluation asks for parameters and executes the code accordingly, returning evaluation result", () => {
+
+});
+
+test("Function call evaluation uses supplied parameters and executes the code accordingly, returning evaluation result", () => {
+
+});
+
+test("Invalid code evaluation returns a error response", () => {
+
+});
